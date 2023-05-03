0e82180b3b0984926c8e2e6ddd2af8dcb400541e
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2023-05-03 14:34:08 +0200

    feat: add empty database module

--- apps/formula-api/src/app.module.ts
+++ apps/formula-api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeasonsModule } from './seasons/seasons.module';
+import { DatabseModule } from './databse/databse.module';
 
 @Module({
-  imports: [SeasonsModule],
+  imports: [SeasonsModule, DatabseModule],
   controllers: [AppController],
   providers: [AppService],
 })
